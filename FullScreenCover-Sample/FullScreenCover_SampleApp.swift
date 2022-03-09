1e06e7d788ef5a24f1d1bf459e56dbe8a7c5eccb
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-03-09 22:10:57 +0900

    Initial Commit

---
+++ FullScreenCover-Sample/FullScreenCover_SampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  FullScreenCover_SampleApp.swift
+//  FullScreenCover-Sample
+//
+//  Created by Hiroaki-Hirabayashi on 2022/03/09.
+//
+
+import SwiftUI
+
+@main
+struct FullScreenCover_SampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ FullScreenCover-SampleTests/FullScreenCover_SampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  FullScreenCover_SampleTests.swift
+//  FullScreenCover-SampleTests
+//
+//  Created by Hiroaki-Hirabayashi on 2022/03/09.
+//
+
+import XCTest
+@testable import FullScreenCover_Sample
+
+class FullScreenCover_SampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
